9950be2535d58fa68c88dc0511d842b0eef4c5d9
Esraa_GamalH <esraa9gamal@gmail.com>
2024-08-12 23:57:21 +0200

    new style edits

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsRequestsService } from '../services/products-requests.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
 
-  constructor(private activatedRoute: ActivatedRoute, private productAPIrequests: ActivatedRoute){}
+  productDetails: any;
+  productinfo: any;
 
-  ngOnInit(){
+  constructor(private activatedRoute: ActivatedRoute,private router: Router , private productAPIrequests: ProductsRequestsService) { }
+
+  ngOnInit() {
     const id = this.activatedRoute.snapshot.params['id'];
-    console.log(id);
-    
-    
+    this.productAPIrequests.getProductDetails(id).subscribe(
+      (data) => (this.productDetails = data), 
+      (error) => {
+        if (error.status === 404){
+          this.router.navigate(['/']);
+        }
+      });
   }
 }
